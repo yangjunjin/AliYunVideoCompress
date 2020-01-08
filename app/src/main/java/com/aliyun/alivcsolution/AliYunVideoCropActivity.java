f3b8f3349d908f7b03bea582af169552eca87bb6
yangjunjin <yangjunjin>
2020-01-08 15:18:35 +0800

    压缩

--- app/src/main/java/com/aliyun/alivcsolution/AliYunVideoCropActivity.java
+++ app/src/main/java/com/aliyun/alivcsolution/AliYunVideoCropActivity.java
@@ -4,10 +4,11 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
-import com.aliyun.common.utils.ToastUtil;
 import com.aliyun.crop.AliyunCropCreator;
 import com.aliyun.crop.struct.CropParam;
 import com.aliyun.crop.supply.AliyunICrop;
@@ -18,6 +19,8 @@ import com.aliyun.svideo.sdk.external.struct.encoder.VideoCodecs;
 
 import org.jetbrains.annotations.Nullable;
 
+import java.util.HashMap;
+
 /**
  * Created by AliYunVideoCropActivity
  * on 2020/1/4
@@ -26,10 +29,6 @@ public class AliYunVideoCropActivity extends Activity implements CropCallback {
 
     private final static String TAG = AliYunVideoCropActivity.class.getSimpleName() + "=======";
     private AliyunICrop crop;
-    private VideoQuality quality = VideoQuality.HD;
-    private VideoCodecs mVideoCodec = VideoCodecs.H264_HARDWARE;
-    private VideoDisplayMode cropMode = VideoDisplayMode.SCALE;
-
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,7 +36,7 @@ public class AliYunVideoCropActivity extends Activity implements CropCallback {
         setContentView(R.layout.alivc_crop_activity_video_crop);
         crop = AliyunCropCreator.createCropInstance(this);
         crop.setCropCallback(this);
-
+        getPlayTime(mInPutPath);
         findViewById(R.id.btnVideoCompress).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -47,39 +46,58 @@ public class AliYunVideoCropActivity extends Activity implements CropCallback {
     }
 
     /**
+     * 初始化参数
+     */
+    private void getPlayTime(String mUri) {
+        android.media.MediaMetadataRetriever mmr = new android.media.MediaMetadataRetriever();
+        try {
+            if (mUri != null) {
+                HashMap<String, String> headers = null;
+                if (headers == null) {
+                    headers = new HashMap<>();
+                    headers.put("User-Agent", "Mozilla/5.0 (Linux; U; Android 4.4.2; zh-CN; MW-KW-001 Build/JRO03C) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 UCBrowser/1.0.0.001 U4/0.8.0 Mobile Safari/533.1");
+                }
+                mmr.setDataSource(mUri, headers);
+            }
+
+            String duration = mmr.extractMetadata(android.media.MediaMetadataRetriever.METADATA_KEY_DURATION);//时长(秒)
+            String width = mmr.extractMetadata(android.media.MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH);//宽
+            String height = mmr.extractMetadata(android.media.MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT);//高
+
+            if (!TextUtils.isEmpty(duration)) mEndTime = Long.parseLong(duration)*1000;
+            if (!TextUtils.isEmpty(width)) outputWidth = Integer.parseInt(width);
+            if (!TextUtils.isEmpty(height)) outputHeight = Integer.parseInt(height);
+            Log.e(TAG, "playtime:" + duration + ",w=" + outputWidth + ",h=" + outputHeight);
+        } catch (Exception ex) {
+            Log.e(TAG, "MediaMetadataRetriever exception " + ex);
+        } finally {
+            mmr.release();
+        }
+    }
+
+    /**
      * 开始压缩
      */
-    String outputPath = "/storage/emulated/0/DCIM/Camera/myVideo3.mp4";
-    String path = "/storage/emulated/0/DCIM/Camera/myVideo1.mp4";
+    String mOutPutPath = "/storage/emulated/0/DCIM/Camera/myVideo0.mp4";
+    String mInPutPath = "/storage/emulated/0/DCIM/Camera/video.mp4";
     int outputWidth = 540;
     int outputHeight = 720;
-    int posX=0;
-    int posY=0;
-    int cropWidth=720;
-    int cropHeight=960;
-    int mStartTime=0;
-    int mEndTime=283868;
-    int frameRate=25;
-    int gop=5;
+    long mEndTime = 283868;
 
     private void startCrop() {
         CropParam cropParam = new CropParam();
-        cropParam.setOutputPath(outputPath);//出的路径
-        cropParam.setInputPath(path);//入的路径
+        cropParam.setOutputPath(mOutPutPath);//出的路径
+        cropParam.setInputPath(mInPutPath);//入的路径
         cropParam.setOutputWidth(outputWidth);
         cropParam.setOutputHeight(outputHeight);
 
-        Rect cropRect = new Rect(posX, posY, posX + cropWidth, posY + cropHeight);
-        cropParam.setCropRect(cropRect);
-        cropParam.setStartTime(mStartTime * 1000);
-        cropParam.setEndTime(mEndTime * 1000);
-        cropParam.setScaleMode(cropMode);
-        cropParam.setFrameRate(frameRate);
-        cropParam.setGop(gop);
-        cropParam.setQuality(quality);
-        cropParam.setVideoCodec(mVideoCodec);
-        cropParam.setFillColor(Color.BLACK);
-        cropParam.setCrf(0);
+        cropParam.setStartTime(0);
+        cropParam.setEndTime(mEndTime);
+        cropParam.setScaleMode(VideoDisplayMode.SCALE);
+        cropParam.setFrameRate(25);
+        cropParam.setGop(5);
+        cropParam.setQuality(VideoQuality.HD);
+        cropParam.setVideoCodec(VideoCodecs.H264_HARDWARE);
         crop.setCropParam(cropParam);
         crop.startCrop();
     }
