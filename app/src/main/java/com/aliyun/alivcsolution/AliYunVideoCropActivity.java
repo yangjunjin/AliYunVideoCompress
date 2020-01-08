1288fc4284cdd07104cbcb4236ee332af817d2dc
yangjunjin <yangjunjin>
2020-01-08 19:31:12 +0800

    压缩

--- app/src/main/java/com/aliyun/alivcsolution/AliYunVideoCropActivity.java
+++ app/src/main/java/com/aliyun/alivcsolution/AliYunVideoCropActivity.java
@@ -3,6 +3,8 @@ package com.aliyun.alivcsolution;
 import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.Rect;
+import android.media.MediaMetadataRetriever;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -20,19 +22,20 @@ import com.aliyun.svideo.sdk.external.struct.snap.AliyunSnapVideoParam;
 
 import org.jetbrains.annotations.Nullable;
 
+import java.io.IOException;
 import java.util.HashMap;
 
 /**
  * Created by AliYunVideoCropActivity
  * on 2020/1/4
  */
-public class AliYunVideoCropActivity extends Activity implements CropCallback {
+public class AliYunVideoCropActivity extends Activity implements CropCallback, MediaPlayer.OnVideoSizeChangedListener {
 
     private final static String TAG = AliYunVideoCropActivity.class.getSimpleName() + "=======";
     private AliyunICrop crop;
     private int ratioMode = AliyunSnapVideoParam.RATIO_MODE_9_16;//宽高比 RATIO_MODE_1_1,RATIO_MODE_3_4，RATIO_MODE_9_16
     private int resolutionMode = AliyunSnapVideoParam.RESOLUTION_540P;//分辨率 RESOLUTION_360P RESOLUTION_480P RESOLUTION_540P RESOLUTION_540P
-
+    private MediaPlayer mPlayer = new MediaPlayer();
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +44,12 @@ public class AliYunVideoCropActivity extends Activity implements CropCallback {
         crop.setCropCallback(this);
         getPlayTime(mInPutPath);
         getWidthHeight();
+        mPlayer.setOnVideoSizeChangedListener(this);
+        try {
+            mPlayer.setDataSource(mInPutPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         findViewById(R.id.btnVideoCompress).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -89,16 +98,16 @@ public class AliYunVideoCropActivity extends Activity implements CropCallback {
                     headers = new HashMap<>();
                     headers.put("User-Agent", "Mozilla/5.0 (Linux; U; Android 4.4.2; zh-CN; MW-KW-001 Build/JRO03C) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 UCBrowser/1.0.0.001 U4/0.8.0 Mobile Safari/533.1");
                 }
-                mmr.setDataSource(mUri, headers);
+//                mmr.setDataSource(mUri, headers);
+                mmr.setDataSource(mUri);
             }
-
             String duration = mmr.extractMetadata(android.media.MediaMetadataRetriever.METADATA_KEY_DURATION);//时长(秒)
             String width = mmr.extractMetadata(android.media.MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH);//宽
             String height = mmr.extractMetadata(android.media.MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT);//高
 
             if (!TextUtils.isEmpty(duration)) mEndTime = Long.parseLong(duration) * 1000;
-            if (!TextUtils.isEmpty(width)) outputHeight = Integer.parseInt(width);
-            if (!TextUtils.isEmpty(height)) outputWidth = Integer.parseInt(height);
+            if (!TextUtils.isEmpty(width)) outputWidth = Integer.parseInt(width);
+            if (!TextUtils.isEmpty(height)) outputHeight = Integer.parseInt(height);
             Log.e(TAG, "playtime1:" + duration + ",w=" + outputWidth + ",h=" + outputHeight);
         } catch (Exception ex) {
             Log.e(TAG, "MediaMetadataRetriever exception " + ex);
@@ -154,6 +163,11 @@ public class AliYunVideoCropActivity extends Activity implements CropCallback {
     @Override
     public void onCancelComplete() {
         Log.d(TAG, "onCancelComplete : ");
+    }
+
+    @Override
+    public void onVideoSizeChanged(MediaPlayer mediaPlayer, int width, int height) {
+        Log.e(TAG,"视频的宽="+width+"，height="+height);
     }
 }
 
